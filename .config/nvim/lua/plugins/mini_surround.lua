e5e5a22f71142b8a247774919349e49361d0c5f9
Josh Smith <jmsmith1018@gmail.com>
2024-04-20 21:00:01 -0700

    Add initial kickstart config

--- .config/nvim/lua/plugins/mini_surround.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "echasnovski/mini.surround",
-  opts = function(_, opts)
-    opts.respect_selection_type = true
-  end,
-}
--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "christoomey/vim-tmux-navigator",
-  keys = {
-    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
-    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
-  },
-}
