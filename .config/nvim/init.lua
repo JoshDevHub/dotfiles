576484c02533ce67a53a14b1c683fd32ce225f19
Josh Smith <jmsmith1018@gmail.com>
2024-05-25 10:29:40 -0700

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -359,10 +359,6 @@ require('lazy').setup({
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
       { 'folke/neodev.nvim', opts = {} },
